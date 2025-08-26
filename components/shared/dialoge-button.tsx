a5ea45a4de31c5c26ecdc4d27069cb03d28e6ed5
Mahdi dehgani <meiti.edu@gmail.com>
2025-08-27 01:58:53 +0330

    Add reusable DialogButton component
    Introduces a DialogButton component that wraps a button and dialog UI, allowing custom content and dialog control via props. This component improves code reuse for dialog-triggering buttons throughout the application.

---
+++ components/shared/dialoge-button.tsx
@@ -0,0 +1,55 @@
+"use client";
+import * as React from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button, buttonVariants } from "../ui/button";
+import { VariantProps } from "class-variance-authority";
+import { Icon } from "@iconify/react/dist/iconify.js";
+
+interface IDialogButtonProps {
+  buttonProps?: React.ComponentProps<"button"> &
+    VariantProps<typeof buttonVariants> & {
+      asChild?: boolean;
+    };
+    title: string;
+    description?: string;
+    children: ({open,setOpen}: {open: boolean, setOpen: React.Dispatch<React.SetStateAction<boolean>>}) => React.ReactNode;
+}
+
+const DialogButton: React.FunctionComponent<IDialogButtonProps> = ({
+  buttonProps = {},
+  title,
+  description,
+  children
+}) => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpenDialog = () => {
+    setOpen(true);
+  };
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <Button {...buttonProps} size={"icon"} className={"bg-transparent text-white" + buttonProps.className }onClick={handleOpenDialog}>
+      <Icon icon="picon:right" />
+      </Button>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>
+           {description}
+          </DialogDescription>
+          <div>
+            {children({open,setOpen})}
+          </div>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default DialogButton;
