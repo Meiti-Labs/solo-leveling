cb2cf97934016d51f0653a974e43c0ffbc1527b3
Mahdi dehgani <meiti.edu@gmail.com>
2025-08-28 05:12:06 +0330

    Add Quest Mongoose model with schema definitions
    Introduces a new Quest model in Mongoose, including embedded schemas for achievements and tasks. The model supports user association, daily quests, deadlines, and tracks task progress and completion.

---
+++ models/quest.model.ts
@@ -0,0 +1,37 @@
+import { Schema, model, models } from "mongoose";
+
+const AchievementSchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String },
+  icon: { type: String, required: true }
+});
+
+const TaskSchema = new Schema({
+  category: { type: String, enum: ["physical","mind","emotional","social","career"]},
+  title: { type: String, required: true },
+  description: String,
+  xp: { type: Number, default: 10 },
+  isCompleted: { type: Boolean, default: false },
+  completedAt: Date,
+  totalProgress: Number,
+  doneProgress: Number
+}, { timestamps: true });
+
+const QuestSchema = new Schema(
+  {
+    userTelegramId: { type: String, required: true, },
+    title: String,
+    description: String,
+    isDaily: { type: Boolean, default: false },
+    deadline: {type: Date},
+    achievement: { type: AchievementSchema, required: false},
+    tasks: {type: [TaskSchema], default: []}
+
+  },
+  { timestamps: true }
+);
+
+// Check if the model already exists
+const QuestModel = models.Quest || model('Quest', QuestSchema);
+
+export default QuestModel;
