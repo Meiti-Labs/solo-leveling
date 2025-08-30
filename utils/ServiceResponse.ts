87dbba1834868063c7488007dc3676c1c40186cb
Mahdi dehgani <meiti.edu@gmail.com>
2025-08-30 21:56:03 +0330

    create a dynamic error handler helper

--- utils/ServiceResponse.ts
+++ utils/ServiceResponse.ts
@@ -5,16 +5,27 @@ export const ApiResponse = {
   success: ({
     messages = ["Operation was successful"],
     data,
-    status = 200
-  }: IApiResponse = {}) => NextResponse.json({
-    resultCode: "Ok",
-    success: true,
-    messages,
-    data,
-  }, {status}) ,
+    status = 200,
+  }: IApiResponse = {}) =>
+    NextResponse.json(
+      {
+        resultCode: "Ok",
+        success: true,
+        messages,
+        data,
+      },
+      { status }
+    ),
   error: ({
     messages = ["Operation failed."],
     resultCode = "Error",
-    status = 400
-  }: IApiResponse = {})  => NextResponse.json({ resultCode, success: false, messages }, {status}),
+    status = 400,
+  }: IApiResponse = {}) =>
+    NextResponse.json({ resultCode, success: false, messages }, { status }),
 };
+
+export function ErrorHandler(err: unknown, ...arg: string[]) {
+  console.log(err);
+  const errorMessage = err instanceof Error ? err.message : String(err || "");
+  return ApiResponse.error({ messages: [errorMessage, ...arg] });
+}
