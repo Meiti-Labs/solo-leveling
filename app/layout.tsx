2b54f5b5f0e72d3f0c805926b8c7b45c9f0aa592
Mahdi dehgani <meiti.edu@gmail.com>
2025-08-30 05:13:31 +0330

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,14 +4,14 @@ import "./globals.css";
 import { Toaster } from "sonner";
 import dynamic from "next/dynamic";
 
-
-const AppProvider = dynamic(()=> import("@/components/app-wrapper"), {ssr: false})
+const AppProvider = dynamic(() => import("@/components/app-wrapper"), {
+  ssr: false,
+});
 
 const FontCinzel = Cinzel({
   variable: "--font-cinzel",
   subsets: ["latin"],
 });
-
 
 export default function RootLayout({
   children,
@@ -20,6 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script defer src="https://telegram.org/js/telegram-web-app.js?59"></script>
+      </head>
       <body className={`${FontCinzel.className}  antialiased dark`}>
         <AppProvider>{children}</AppProvider>
         <Toaster />
