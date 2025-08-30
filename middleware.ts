eda6663e82d7d5a922dc88c1aac174351f386aa2
Mahdi dehgani <meiti.edu@gmail.com>
2025-08-30 18:10:30 +0330

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,19 @@
+import { validate } from "@telegram-apps/init-data-node";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { ApiResponse } from "./utils/ServiceResponse";
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
-  if(pathname.startsWith("/api/secure/")){
-    const token = request.cookies.get("token")?.value || "";
-    try{
-        if(token)
-        return NextResponse.next();
-    } catch (error) {
-        return new NextResponse(`${error as string}` , { status: 401 });
+  if (pathname.startsWith("/api/secure/")) {
+    const initData =
+      request.headers.get("authorization")?.replace(/^tma\s+/i, "") || "";
+
+    if (initData) {
+      return NextResponse.next();
+    } else {
+      return ApiResponse.error({ messages: ["Telegram initial data faild"] });
     }
   }
   return NextResponse.next();
