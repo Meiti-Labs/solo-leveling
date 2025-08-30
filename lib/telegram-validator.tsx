a49426bec622e45a38be90c63e612e5746149f6b
Mahdi dehgani <meiti.edu@gmail.com>
2025-08-30 18:10:28 +0330

    Create telegram-validator.tsx

---
+++ lib/telegram-validator.tsx
@@ -0,0 +1,15 @@
+import { validate } from "@telegram-apps/init-data-node";
+import { NextRequest } from "next/server";
+
+export default function VerifyTelegramRequest(
+  request: NextRequest
+): string | null {
+  const initData =
+    request.headers.get("authorization")?.replace(/^tma\s+/i, "") || "";
+  validate(initData, process.env.TELEGRAM_BOT_TOKEN!);
+  const params = new URLSearchParams(initData);
+  const userRaw = params.get("user");
+  const user = userRaw ? JSON.parse(userRaw) : null;
+  const userId = user?.id;
+  return userId;
+}
