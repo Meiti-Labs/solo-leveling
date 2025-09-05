dde8763f22a38152ea92771e2f962a652e08433e
Mahdi dehgani <meiti.edu@gmail.com>
2025-09-06 01:39:17 +0330

    Update Create Page

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -6,9 +6,15 @@ import * as React from "react";
 
 export default function Create() {
   return (
-    <div>
+    <div className="flex flex-col gap-3">
       <Box className="p-3 bg-[#BE99FE]/20 flex justify-between items-center">
         Create quest{" "}
+        <DialogButton title="create quest">
+          {({ setOpen }) => <CreateQuestForm setOpen={setOpen} />}
+        </DialogButton>
+      </Box>
+      <Box className="p-3 bg-[#BE99FE]/20 flex justify-between items-center">
+        Create group quest{" "}
         <DialogButton title="create quest">
           {({ setOpen }) => <CreateQuestForm setOpen={setOpen} />}
         </DialogButton>
